33efc9fccd462319620bb6db119a17969061a657
Fayzullo07 <jorayevfayzillo77@gmail.com>
2024-04-27 22:56:04 +0500

    vercel 1

--- configs/auth.ts
+++ configs/auth.ts
@@ -1,48 +1,53 @@
+import NextAuth, { AuthOptions } from "next-auth";
+import CredentialsProvider from 'next-auth/providers/credentials';
+import bcrypt from "bcryptjs";
 import connectMongoDB from "@/lib/mongodb";
 import User from "@/models/userModel";
-import { AuthOptions } from "next-auth";
-import CredentialsProvider from 'next-auth/providers/credentials';
-import bcrypt from "bcryptjs"
 
 export const authOptions: AuthOptions = {
     providers: [
         CredentialsProvider({
-            name: "credentials",
-            credentials: {},
-            async authorize(credentials: any) {
+            name: "Credentials",
+            credentials: {
+                login: { label: "Login", type: "text", placeholder: "Enter your login" },
+                password: { label: "Password", type: "password", placeholder: "Enter your password" }
+            },
+            async authorize(credentials) {
+                if (!credentials) {
+                    return null;
+                }
+
                 await connectMongoDB();
+
                 const { login, password } = credentials;
+
                 try {
                     const user = await User.findOne({ login });
-
                     if (!user) {
-                        return null
+                        // If no user is found, return null to indicate failed login
+                        return null;
                     }
 
                     const passwordMatch = await bcrypt.compare(password, user.password);
-
                     if (!passwordMatch) {
-                        return null
-                    }
-                    console.log(user);
-                    if (user) {
-                        return user
-                    } else {
-                        return null
+                        // If the password doesn't match, return null to indicate failed login
+                        return null;
                     }
 
+                    // If password matches, return the user object which will be serialized in the JWT
+                    return user;
                 } catch (error) {
-                    console.log(error);
-
+                    console.error("Login error:", error);
+                    return null;  // Ensure returning null on error to prevent login
                 }
             }
         })
     ],
     session: {
-        strategy: 'jwt'
+        strategy: 'jwt'  // Using JSON Web Tokens for session strategy
     },
-    secret: process.env.NEXTAUTH_SECRET,
+    secret: process.env.NEXTAUTH_SECRET,  // Secret used to encrypt the JWT
     pages: {
-        signIn: "/login"
+        signIn: "/login"  // Custom sign-in page path
     }
 };
