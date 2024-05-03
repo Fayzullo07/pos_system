b7cc6aeb434830b0c2e73c648786d18048c1eb9e
Fayzullo07 <jorayevfayzillo77@gmail.com>
2024-05-03 17:57:14 +0500

    title

--- src/app/api/order_numbers/route.tsx
+++ src/app/api/order_numbers/route.tsx
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 export const GET = async (req: NextRequest) => {
     await connectMongoDB();
     try {
-        let order_numbers = await OrderNumber.find({ isActive: true });
+        const order_numbers = await OrderNumber.find({ isActive: true });
         return NextResponse.json({ order_numbers }, { status: 200 })
     } catch (error) {
         return NextResponse.json({ order_numbers: [] }, { status: 500 })
